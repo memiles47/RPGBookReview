813b5b67e62cf21f362543f40fcb556110a18acb
Michael E Miles <michael@miles-me.net>
2018-03-12 12:29:37 -0500

    Adding Base Scripts
    _GameMaster and BaseCharacter classes added.

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts
+{
+    public class GameMaster : MonoBehaviour {
+
+        // Use this for initialization
+        void Start ()
+        {
+		
+        }
+	
+        // Update is called once per frame
+        void Update ()
+        {
+		
+        }
+
+        void StartGame()
+        {
+            //You should put in the name of the scene that represents your
+            //start level
+            SceneManager.LoadScene("Awakening");
+        }
+    }
+}
