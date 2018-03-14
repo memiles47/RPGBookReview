4a281e5a4c6660b8c1b664fb02d0d0cd6cdc9d3f
Michael E Miles <michael@miles-me.net>
2018-03-14 08:07:42 -0500

    Add Game Master and script
    Added Game master empty Game Object and script. Added canvas for start of menu and tested game start button.

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -3,25 +3,25 @@ using UnityEngine.SceneManagement;
 
 namespace Assets.Scripts
 {
-    public class GameMaster : MonoBehaviour {
+	public class GameMaster : MonoBehaviour {
 
-        // Use this for initialization
-        void Start ()
-        {
-		
-        }
+		// Use this for initialization
+		void Start ()
+		{
+		    DontDestroyOnLoad(this);
+		}
 	
-        // Update is called once per frame
-        void Update ()
-        {
+		// Update is called once per frame
+		void Update ()
+		{
 		
-        }
+		}
 
-        void StartGame()
-        {
-            //You should put in the name of the scene that represents your
-            //start level
-            SceneManager.LoadScene("Awakening");
-        }
-    }
+		public void StartGame()
+		{
+			//You should put in the name of the scene that represents your
+			//start level
+			SceneManager.LoadScene("Awakening");
+		}
+	}
 }
